96fec1036729f968d4f3b57115868942ebdb6531
Sadeeq Williams <sadeeqwilliams@Sadeeqs-MacBook-Pro.local>
2023-04-13 22:54:50 +0200

    Console app sln

---
+++ POE/Part 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using POE;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int ch = 4;
+        Recipe obj = new Recipe();
+
+        obj.Display();
+
+        do
+        {
+            Console.WriteLine("\nPress \n1 to Scale the recipe:\n 2 to reset the recipe,\n 3 to clearthe recipe,\n 4 to exit");
+            ch = Convert.ToInt32(Console.ReadLine());
+            switch (ch)
+            {
+                case 1:
+
+                    obj.Scalling();
+                    break;
+
+                case 2:
+
+                    obj.Reset();
+                    break;
+
+                case 3:
+
+                    obj.Clear();
+                    break;
+
+                case 4:
+
+                    Console.WriteLine("Thank You");
+                    break;
+            }
+
+        } while (ch != 4);
+    }
+}
---
+++ POE/Part 1/Recipe.cs
@@ -0,0 +1,144 @@
+﻿using System;
+namespace POE
+{
+    public class Recipe
+    {
+        // Initialize recipe variables
+        int numIngredients = 0;
+        int numSteps = 0;
+        string[] ingredients; //Array to store ingredient names
+        double[] quantities; //Array to store ingredient quantities
+        string[] units;     //Array to store ingredient units
+        string[] steps;    //Array to store step descriptions
+        double[] quanty;
+        double q;
+        public void Display()
+        {
+
+            // Prompt user for recipe details
+            Console.WriteLine("Enter the number of ingredients:");
+
+            numIngredients = Convert.ToInt32(Console.ReadLine());
+            ingredients = new string[numIngredients];
+            quantities = new double[numIngredients];
+            units = new string[numIngredients];
+            quanty = new double[numIngredients];
+
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine("Enter the name of ingredient " + $"{i + 1} :");
+
+                ingredients[i] = Console.ReadLine();
+
+                Console.WriteLine("Enter the quantity of ingredient " + $"{i + 1}:");
+                q = Convert.ToDouble(Console.ReadLine());
+                quantities[i] = q;
+                quanty[i] = quantities[i];
+
+
+                Console.WriteLine("Enter the unit of measurement for ingredient " + $"{i + 1}:");
+                units[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter the number of steps:");
+            numSteps = Convert.ToInt32(Console.ReadLine());
+            steps = new string[numSteps];
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine("Enter the description of step " + $"{i + 1}:");
+                steps[i] = Console.ReadLine();
+            }
+
+            // Display the recipe
+            Console.WriteLine("\nIngredients:\n________________________________");
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"{i + 1}" + ". " + quantities[i] + " " + units[i] + " " + ingredients[i]);
+            }
+            Console.WriteLine("________________________________\nSteps:\n________________________________");
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"{i + 1}" + ". " + steps[i]);
+            }
+
+        }
+
+
+        //Prompt the user to either double half triple the recipe
+        public void Scalling()
+
+        {
+            double Factor;
+
+
+            Console.WriteLine("\nEnter the scale factor (0.5, 2, or 3): ");
+            Factor = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                quanty[i] *= Factor;
+            }
+
+            Console.WriteLine("\nScaled recipe:");
+            Console.WriteLine("______________________________\nIngredients:\n________________________________");
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"{i + 1}" + ". " + quanty[i] + " " + units[i] + " " + ingredients[i]);
+            }
+
+            Console.WriteLine("________________________________\nSteps:\n________________________________");
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"{i + 1}" + ". " + steps[i]);
+            }
+
+        }
+
+        // resets the recipe quantities to the original input
+        public void Reset()
+        {
+
+            Console.WriteLine("\nOriginal recipe:\n________________________________");
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+
+                Console.WriteLine(quantities[i] + units[i] + " " + ingredients[i]);
+
+
+            }
+            Console.WriteLine("________________________________\nSteps:\n________________________________");
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"{i + 1}" + ". " + steps[i]);
+            }
+        }
+
+        //Allows the user to clear all the data stored in the arrays
+        public void Clear()
+        {
+
+            ingredients = new string[0];
+            quantities = new double[0];
+            units = new string[0];
+            steps = new string[0];
+
+            Console.WriteLine("\nAll data cleared press 1 to enter a new recipe or 2 to return to menu:");
+            int scaleOption = Convert.ToInt32(Console.ReadLine());
+
+            if (scaleOption == 1)
+            {
+                Display();
+            }
+            else Console.WriteLine("Returning to menu");
+        }
+
+    }
+
+}
+
+
+
+
