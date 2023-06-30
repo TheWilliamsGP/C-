a9df377a569f02a0afc44a7d224e2587abf882e2
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2023-06-30 21:18:09 +0200

    Update Ingredients.cs

--- POE/POE/Ingredients.cs
+++ POE/POE/Ingredients.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace POE
 {
-    internal class Ingredients
+
+    // class to store all the getters and setters for the ingredient details
+    public class Ingredients
     {
+        public string ingredient { get; set; }
+        public double quantities { get; set; }
+        public string units { get; set; }
+        public double Quantity { get; set; }
+        public int Calories { get; set; }
+        public string foodGroup { get; set; }
+        
+
+        public Ingredients(string ingredients, double quantity, string unit, int calories, string foodGrp)
+        {
+
+           ingredient = ingredients;
+           quantities = quantity;
+           units = unit;
+           Quantity = quantity;
+           Calories = calories;
+           foodGroup = foodGrp;
+        }
     }
+   
 }
