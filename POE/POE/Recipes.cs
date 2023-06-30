d6b73e662fa9b337f4dcce60585f959e7387710c
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2023-06-30 21:18:51 +0200

    Update Recipes.cs

--- POE/POE/Recipes.cs
+++ POE/POE/Recipes.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace POE
 {
-    internal class Recipes
+    // class to create list of ingredients, steps and recipe name
+    public class Recipes
     {
+        public string recipename { get; set; }
+        public string steps { get; set; }
+
+        public List<Ingredients> ingredients { get; set; }
+        public List<Steps> STEPS { get; set; }
+
+        public Recipes() { 
+        ingredients = new List<Ingredients>();
+        STEPS = new List<Steps>();
+          
+        
+        }
     }
 }
