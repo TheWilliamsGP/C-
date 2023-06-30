9646ae0ecabf3b2b3f2e9f85ff5cddb19ec37bbc
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2023-06-30 21:19:47 +0200

    Update StepsForm.cs

--- POE/POE/StepsForm.cs
+++ POE/POE/StepsForm.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace POE
 {
+
+
     public partial class StepsForm : Form
     {
-        public StepsForm()
+
+        private Recipes recipe;
+        public List<Steps> Step;
+        public StepsForm(Recipes recipes)
         {
             InitializeComponent();
+            this.recipe = recipes;
+            Step = new List<Steps>();
+            
+        }
+
+        private void Add_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //stores the steps in the list
+                string step = StepstextBox.Text;
+                Steps steps = new Steps(step);
+                Step.Add(steps);
+                MessageBox.Show("Steps added successfully", "Added", MessageBoxButtons.OK);
+                StepstextBox.Text = "";
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Error! Please fill in the fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            recipe.STEPS.AddRange(Step);
+            Close();
         }
     }
 }
