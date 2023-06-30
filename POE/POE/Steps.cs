c8b5cdc276d0e5fe6e5095dad694e0eb8e324915
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2023-06-30 21:21:01 +0200

    Update Steps.cs

--- POE/POE/Steps.cs
+++ POE/POE/Steps.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    // class to store all the steps 
+    public class Steps
+    {
+        public string step {get;set;}
+
+        public Steps(string Step)
+        {
+            step = Step; 
+        }
+    }
+   
+
+}
 
