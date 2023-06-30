abfcedd53409c36955eabf9acd7fe4fc76363b3b
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2023-06-30 21:14:45 +0200

    Update AddRecipes.cs

--- POE/POE/AddRecipes.cs
+++ POE/POE/AddRecipes.cs
@@ -12,9 +12,32 @@ namespace POE
 {
     public partial class AddRecipes : Form
     {
+        public event Action<Recipes> RecipeAdded;
         public AddRecipes()
         {
             InitializeComponent();
+        }
+        //button to save the recipe name 
+        private void Save_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string recipeName = RecipeName_textBox.Text;
+                Recipes recipe = new Recipes() { recipename = recipeName };
+                RecipeAdded?.Invoke(recipe);
+                MessageBox.Show("Recipe Name successfully stored", "Added", MessageBoxButtons.OK);
+
+                IngredientForm ingredientForm = new IngredientForm(recipe);
+                ingredientForm.Show();
+
+                Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error! please fill in the correct fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
