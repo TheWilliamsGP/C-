3d396f5c61d100a8a11b49bb75a0c9fb32f5a184
TheWilliamsGP <102786528+TheWilliamsGP@users.noreply.github.com>
2023-06-30 21:16:16 +0200

    Update Form1.cs

--- POE/POE/Form1.cs
+++ POE/POE/Form1.cs
@@ -2,22 +2,41 @@ namespace POE
 {
     public partial class Form1 : Form
     {
-        private List<Recipes> recipes;
+        // create a new list thats stores all the ingredients under a specific name typed by the user
 
+        private List<Recipes> recipes;
         public Form1()
         {
             InitializeComponent();
-            recipes = new List<Recipes> ();
+            recipes = new List<Recipes>();
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        
+        //Button to take the user to add a new recipe
         private void AddRecipe_button_Click(object sender, EventArgs e)
         {
+            try
+            {
+                AddRecipes AR = new AddRecipes();
+                AR.RecipeAdded += AddRecipeForm_RecipeAdded;
+                AR.Show();
+            }catch(Exception ex) { }
+        }
 
+        private void AddRecipeForm_RecipeAdded(Recipes recipe)
+        {
+            recipes.Add(recipe);
+        }
+        //button to display the stored recipes
+        private void Display_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+
+                DisplayForm DR = new DisplayForm(recipes);
+                DR.Show();
+            }catch(Exception ex) { }
         }
     }
 }
